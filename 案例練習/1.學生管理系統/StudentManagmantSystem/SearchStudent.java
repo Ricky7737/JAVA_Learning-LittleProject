3f13b16ca85d891430ed500ea54ab2e25639e5d1
AbaoRicky <58324475+Ricky7737@users.noreply.github.com>
2024-04-19 00:32:10 +0800

    Update SearchStudent.java

--- 案例練習/1.學生管理系統/StudentManagmantSystem/SearchStudent.java
+++ 案例練習/1.學生管理系統/StudentManagmantSystem/SearchStudent.java
@@ -1,16 +1,18 @@
-package RealCase;
+package RealCase.StudentManagmantSystem;
 
 import java.util.ArrayList;
 
 public class SearchStudent {
     public static void queryStudent(ArrayList<student> list) {
-        // 4.1 如果沒有學生訊資訊，就先提示他們先建立學生訊息
+        // 1 如果沒有學生訊資訊，就先提示他們先建立學生訊息
         if (list.isEmpty()) {
             System.out.println("當前沒有學生訊息，麻煩添加");
-            // 跳出迴圈
-            return;
-        // 4.2 如果有學生訊息，就列出學生訊息
+            // 執行到 return 就不會繼續執行
+            // 並且終止 queryStudent 這個方法
+            return;        
         } 
+        
+        // 2 如果有學生訊息，就列出學生訊息
         System.out.println("id\t姓名\t性別\t生日");
         for (int i = 0; i < list.size(); i++) {
             // 取得學生訊息
