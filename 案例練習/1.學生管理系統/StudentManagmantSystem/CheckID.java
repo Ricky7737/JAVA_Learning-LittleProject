1345f2bad6ed6f551ce6cfe4e0f88b9661713553
AbaoRicky <58324475+Ricky7737@users.noreply.github.com>
2024-04-19 00:57:41 +0800

    2024-4-19-00:56上傳部分功能
    1. 編寫 刪除功能跟修改資料功能
    2. 完成文檔說明

---
+++ 案例練習/1.學生管理系統/StudentManagmantSystem/CheckID.java
@@ -0,0 +1,21 @@
+/* 因為確定 ID 這個，在 添加 ID 時，要判斷 ID 是否已經存在
+* 然後刪除 ID 時，也要判斷 ID 是否存在
+* 所以將這個方法獨立出一個類別，方便重複使用
+*/
+
+package RealCase.StudentManagmantSystem;
+import java.util.ArrayList;
+
+// 這個類別用來判斷 id 是否已經存在
+public class CheckID {
+    // 這個方法用來判斷 id 是否已經存在
+    // 若存在，則回傳 true，否則回傳 false
+    public static boolean checkId(ArrayList<student> list, String id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
